4cd33aabeab5f3c28c590fadf67ac0f3d41d92f1
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-09 16:34:57 +0100

    WIP: setup app as class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>placeholder app</p>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  render() {
+    return (
+      <div className="App">
+        <p>placeholder app</p>
+      </div>
+    );
+  }
 }
 
 export default App;

46351cccf0b7f4ffb13db301f5d2a11e92ced1c7
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-09 18:04:54 +0100

    Completed: MVP, mapping over array of objects received from restricted data upon new user signup

--- client/src/RegistrationForm.js
+++ client/src/RegistrationForm.js
@@ -2,27 +2,32 @@ import React, { useState, useEffect } from 'react'
 import { withFormik, Form, Field } from 'formik'
 import * as Yup from 'yup'
 import axios from 'axios'
+import MealCard from './MealCard';
 
 const RegistrationForm = ({ values, errors, touched, status }) => {
   const [data, setData] = useState([])
 
   useEffect(() => {
     if (status) {
-      setData([status])
+      setData(status)
       console.log(data)
     }
   }, [status])
 
   return (
-    <Form>
-      <Field type="username" name='username' placeholder='Username' />
-      <Field type='password' name='password' placeholder='Password' />
-      <button type='submit' data-testid='formSubmitButton'>Sign Up</button>
-      <div data-test-id='error-messages'>
-        {touched.username && errors.username && <p data-testid='usernameErrorMessage'>{errors.username}</p>}
-        {touched.password && errors.password && <p>{errors.password}</p>}
-      </div>
-    </Form>
+    <>
+      <Form>
+        <Field type="username" name='username' placeholder='Username' />
+        <Field type='password' name='password' placeholder='Password' />
+        <button type='submit' data-testid='formSubmitButton'>Sign Up</button>
+        <div data-test-id='error-messages'>
+          {touched.username && errors.username && <p data-testid='usernameErrorMessage'>{errors.username}</p>}
+          {touched.password && errors.password && <p>{errors.password}</p>}
+        </div>
+      </Form>
+      {data.map(meal => <MealCard key={meal.name} name={meal.name} course={meal.course} technique={meal.technique}/>)}
+      <MealCard />
+    </>
   )
 }
 
